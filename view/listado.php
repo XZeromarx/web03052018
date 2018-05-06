ffe0b7c0d0da4c8efd55c1e5b2f66f127a91fbf5
yeniferosorio <sofiaa_yenifer@hotmail.com>
2018-05-06 15:22:37 -0400

    se empieza a crear lista

--- view/listado.php
+++ view/listado.php
@@ -1,1 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../model/bulma-0.7.1/css/bulma.min.css">
+        <title>Listado de Personas</title>
+    </head>
+    <body>
+        <table class="table">
+  <tr>
+    <th>Hoy</th>
+    <th>Mañana</th>
+    <th>Martes</th>
+  </tr>
+  <tr>
+    <td>Soleado</td>
+    <td>Mayormente soleado</td>
+    <td>Parcialmente nublado</td>
+  </tr>
+  <tr>
+    <td>19°C</td>
+    <td>17°C</td>
+    <td>12°C</td>
+  </tr>
+  <tr>
+    <td>E 13 km/h</td>
+    <td>E 11 km/h</td>
+    <td>S 16 km/h</td>
+  </tr>
+</table>
+    </body>
+</html>
