d209dad9f1d554a038846d226d1c202fc1eede76
yeniferosorio <sofiaa_yenifer@hotmail.com>
2018-05-06 00:31:08 -0400

    se agrego textfiel y boton y una barra de proceso

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registros de personas</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+        <title>Registros de personas</title>
+    </head>
+    <body>
+     <div class="container is-centered">
+     <div class="box">
+        <div class="control">
+        <input class="input is-focused" type="text"  placeholder="Ingrese Nombre" >
+        </div>
+        <br>
+                    
+    <div class="control">
+        <input class="input is-focused" type="text"  placeholder="Ingrese Etiquetas" >  </div>
+        <br>
+        
+        <a class="button is-primary is-outlined">Registrar</a>
+        <br>
+        </br>
+         <progress class="progress is-primary" value="0" max="100">30%</progress>   
+                
+          
+  
+    </div>
+    </div>
+   
     
-</body>
+    
+       
+   
+    </body>
 </html>
